5e232b7b5c66f1b0058215628941ba0e260ab036
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2022-09-22 18:31:46 -0300

    Add files via upload

---
+++ constructors/Car.java
@@ -0,0 +1,24 @@
+package videoaulas;
+
+public class Car {
+	
+	String brand;
+	String model;
+	int passengersNumber;
+	double fuelCapacity;
+	double fuelConsumption;
+
+	public Car() {
+		
+	}
+	
+	public Car(String brand, String model, int passengersNumber, double fuelCapacity, double fuelConsumption) {
+		super();
+		this.brand = brand;
+		this.model = model;
+		this.passengersNumber = passengersNumber;
+		this.fuelCapacity = fuelCapacity;
+		this.fuelConsumption = fuelConsumption;
+	}
+	
+}
---
+++ constructors/Constructor.java
@@ -0,0 +1,15 @@
+package videoaulas;
+
+public class Constructor {
+	
+	public static void main(String[] args) {
+		Car van = new Car("fiat", "ducato", 10, 45, 9);
+		
+		System.out.println("Marca: "+van.brand);
+		System.out.println("Modelo: "+van.model);
+		System.out.println("Numero de passageiros: "+van.passengersNumber);
+		System.out.println("Capacidade de combustivel: "+van.fuelCapacity);
+		System.out.println("Consumo de combustivel: "+van.fuelConsumption);
+	}
+	
+}
