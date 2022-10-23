76b5dde8737074664715059dece05adc7c36d907
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2022-10-23 16:32:51 -0300

    Update Escola.java

--- Herança/Escola.java
+++ Herança/Escola.java
@@ -4,6 +4,14 @@ public class Escola {
 
 	public static void main(String[] args) {
 		
+		Aluno aluno = new Aluno();
+		Professor professor = new Professor();
+		
+		aluno.setEndereco("Rua 41 , Alameda verde");
+		professor.setEndereco("Rua 4, parque alburqueque");
+		
+		System.out.println(aluno.obterEndereco());
+		System.out.println(professor.obterEndereco());
 
 	}
 
