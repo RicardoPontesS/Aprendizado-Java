dca70459e64d4a7dbe56385d6f4b5b894518d918
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2022-12-03 16:06:20 -0300

    Update DiaSemana.java

--- Enum/DiaSemana.java
+++ Enum/DiaSemana.java
@@ -1,8 +1,23 @@
 package videoaulas;
 
 public enum DiaSemana {
+
+	SEGUNDA(1), TERCA(2), QUARTA(3), QUINTA(4), SEXTA(5), SABADO(6), DOMINGO(7);
 	
-	SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO;
-	
-	
+	private int valor;
+
+	DiaSemana(int valor) {
+
+		this.valor = valor;
+
+	}
+
+	public int getValor() {
+		return valor;
+	}
+
+	public void setValor(int valor) {
+		this.valor = valor;
+	}
+
 }
