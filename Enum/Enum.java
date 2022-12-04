7e8bff5e4586b25bab4285de3f24dfc61aa7d794
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2022-12-04 13:39:29 -0300

    Update Enum.java

--- Enum/Enum.java
+++ Enum/Enum.java
@@ -9,5 +9,19 @@ public class Enum {
 		
 		System.out.println(dia.toString() +"-"+dia.getValor());
 		System.out.println(sexo.toString()+"-"+sexo.getSexo());
+		
+		/*
+		
+		Formulario[] forms = Formulario.values(); //Retorna um array de todos os valores dentro do Enum
+		
+		for(int i = 0; i<forms.length; i++) { 
+			System.out.println(forms[i]);
+		}
+		
+		for(Formulario form: Formulario.values()) {
+			System.out.println(form);
+		}
+		
+		/*
 	}
 }
