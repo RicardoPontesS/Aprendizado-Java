7ee485878fb55991ebca715cf00e622f566ba0a2
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2022-12-07 10:57:47 -0300

    Update Wrapper.java

--- Wrapper.java
+++ Wrapper.java
@@ -14,16 +14,15 @@ public class TesteWrapper {
 		boolean flag = true;
 		char a = 'a'; 
 		*/
-		
-		Short num = new Short((short) 1);
-		Byte num1 = new Byte((byte) 10);
-		Integer num2 = new Integer(100);
-		Long num3 = new Long(10000);
-		Float num4 = new Float(3.5f);
-		Double num5 = new Double(3.5);
-		Boolean num6 = new Boolean(true);
-		Character num7 = new Character('c');
-		
+
+		Short num = Short.valueOf("1");
+		Byte num1 = Byte.valueOf("10");
+		Integer num2 = Integer.valueOf(100);
+		Long num3 = Long.valueOf(10000);
+		Float num4 = Float.valueOf(3.5f);
+		Double num5 = Double.valueOf(3.5);
+		Boolean num6 = Boolean.valueOf(true);
+		Character num7 = Character.valueOf('c');
 		
 		//conversão de valores na própria classe Wrapper
 		System.out.println(num4.longValue());
