355ebc8547a1289cb156ff4548feb3822781fd0c
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2022-12-07 10:45:55 -0300

    Add files via upload

---
+++ TesteWrapper.java
@@ -0,0 +1,29 @@
+package videoaulas;
+
+public class TesteWrapper {
+
+	public static void main(String[] args) {
+		
+		/* Exemplos de tipos primitivos básicos
+		short num1 = 1;
+		byte num2 = 10;
+		int num3 = 100;
+		long num4 = 10000;
+		float num5 = 4.5f;
+		double num6 = 4.5;
+		boolean flag = true;
+		char a = 'a'; 
+		*/
+		
+		Short num = new Short((short) 1);
+		Byte num1 = new Byte((byte) 10);
+		Integer num2 = new Integer(100);
+		Long num3 = new Long(10000);
+		Float num4 = new Float(3.5f);
+		Double num5 = new Double(3.5);
+		Boolean num6 = new Boolean(true);
+		Character num7 = new Character('c');
+		
+	}
+
+}
