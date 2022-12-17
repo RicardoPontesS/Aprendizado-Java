f64a800c074e110f0bb77618bc7504c46e71c9aa
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2022-12-17 18:36:55 -0300

    Add files via upload

---
+++ Anotações/Informacao.java
@@ -0,0 +1,7 @@
+package videoaulas;
+
+public @interface Informacao {
+
+	String autor() default "" ;
+	String titulo() default "" ;
+}
---
+++ Anotações/TesteAnotacao.java
@@ -0,0 +1,11 @@
+package videoaulas;
+
+@Informacao(autor = "Ricardo Pontes", titulo = "Curso de Java")
+ 
+public class TesteAnotacao {
+
+	public static void main(String[] args) {
+
+	}
+
+}
