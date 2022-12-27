6675bd4ff147fcd0ca45be7716fe9fdfa45b99ed
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2022-12-27 15:49:47 -0300

    Update Vetor.java

--- Estrutura de Dados com Java/Vetor.java
+++ Estrutura de Dados com Java/Vetor.java
@@ -1,5 +1,7 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class Vetor {
 
 	private String[] elementos;
@@ -16,6 +18,23 @@ public class Vetor {
 
 	}
 
+	public String toString() {
+		
+		StringBuilder s = new StringBuilder();
+		s.append("[");
+		
+		for(int i=0;i<this.tamanho-1;i++) {
+			s.append(this.elementos[i]);
+			s.append(" ,");
+		}
+		
+		if(this.tamanho>0) {
+			s.append(this.elementos[this.tamanho-1]);
+		}
+		s.append("]");
+		return s.toString();
+	}
+	
 //---------------------------------------------
 	public Vetor(int capacidade) {
 		elementos = new String[capacidade];
@@ -30,4 +49,7 @@ public class Vetor {
 		this.elementos = elementos;
 	}
 
+	public int getTamanho() {
+		return tamanho;
+	}
 }
