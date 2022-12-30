56ae59fef8fa1f88aa243053000c8b0e6536aab9
Ricardo <105815500+RicardoPontesS@users.noreply.github.com>
2022-12-30 13:09:22 -0300

    Update Stacks.java

--- Estrutura de Dados com Java/Stacks.java
+++ Estrutura de Dados com Java/Stacks.java
@@ -1,5 +1,20 @@
 package stacks;
 
-public class Stacks {
+import java.util.Stack;
+
+public class Pilha {
+
+	public static void main(String[] args) {
+		Stack<Integer> stack = new Stack<Integer>();
+		System.out.println(stack);
+		stack.push(4);
+		stack.push(8);
+		stack.push(10);
+		System.out.println("ToString"+stack);
+		System.out.println("Retorna o tamanho atual da pilha: "+stack.size());
+		System.out.println("Retorna o elemento no topo da pilha: "+stack.peek());
+		/* Remove o elemento no topo da pilha*/stack.pop();
+		System.out.println(stack);
+	}
 
 }
